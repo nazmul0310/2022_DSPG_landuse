6cd1315daacc3dd8ff4a376df270c4886cdd8c6b
Huda <huda@vt.edu>
2022-07-12 10:00:52 -0400

    Uploading plotting of hotspots

---
+++ Review/Huda/Mapping_Hotspot_Data.R
@@ -0,0 +1,42 @@
+library(sf)
+library(ggplot2)
+library(raster)
+library(leaflet) 
+
+# Reading each year's parcellation hotspots
+
+gl_hs_19 <- st_read(  "gooch_hotspot_19.shp")
+gl_hs_20 <- st_read(  "gooch_hotspot_20.shp")
+gl_hs_21 <- st_read(  "gooch_hotspot_21.shp")
+gl_hs_22 <- st_read(  "gooch_hotspot_22.shp")
+
+# Plotting the files together in ggplot
+ggplot() + 
+  geom_sf(data = gl_hs_19, fill = "red", color = "NA", alpha = 1/10) +
+  geom_sf(data = gl_hs_20, fill = "red", color = "NA", alpha = 1/10) +
+  geom_sf(data = gl_hs_21, fill = "red", color = "NA", alpha = 1/10) +
+  scale_color_manual(values = road_colors) +
+  scale_fill_manual(values = "black") +
+  ggtitle("Parcellation Hotspots in Goochland", subtitle = "2018 ~ 2022") + 
+  coord_sf()
+
+
+# Reprojecting as leaflet only works in this projection system
+gl_hs_19_rp <- st_transform(gl_hs_19, 4326)
+gl_hs_20_rp <- st_transform(gl_hs_20, 4326)
+gl_hs_21_rp <- st_transform(gl_hs_21, 4326)
+gl_hs_22_rp <- st_transform(gl_hs_22, 4326)
+
+
+leaflet(gl_hs_22_rp) %>%
+
+  addPolygons(
+    stroke = FALSE, # remove polygon borders
+    #fillColor = ~pal_fun(gridcode), # set fill color with function from above and value
+    fillOpacity = 0.5, smoothFactor = 0.5,
+  ) %>% # Works until this point but doesn't add tiles
+
+  addTiles(group = "OSM") %>%
+  addProviderTiles("CartoDB.DarkMatter", group = "Carto")
+  
+  # Need to put multiple shapefiles with leaflet and have the tiles in the background
