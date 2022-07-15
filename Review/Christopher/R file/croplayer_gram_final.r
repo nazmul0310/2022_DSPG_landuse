7c9cccf91efd865904d82640787e31fce9d5d270
Christopher Vest <cvest@stu.jsu.edu>
2022-07-15 09:03:51 -0400

    Plotly fixes

--- Review/Christopher/R file/croplayer_gram_final.r
+++ Review/Christopher/R file/croplayer_gram_final.r
@@ -6,7 +6,7 @@ library(tidyverse)
 library(dplyr)
 options(scipen = 999)   
 
-croplayer1 <- read.csv("C:/Users/Christopher Vest/Documents/Land_use_project/2022_DSPG_Landuse/Review/Christopher/Croplayer/ag_analysis.csv")
+croplayer1 <- read.csv("C:/Users/Christopher Vest/Documents/Land_use_project/2022_DSPG_landuse/ShinyApp/data/ag_analysis.csv")
 
 p_21 <- croplayer1 %>% 
   filter(County == "Powhatan", Year==2021) %>%
@@ -17,11 +17,8 @@ p_21 <- croplayer1 %>%
   theme(axis.text.y = element_text(hjust=0)) +
   theme(legend.position = "none") + 
   scale_fill_viridis() + 
-  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type") %>% 
-plot_ly(p_21, tooltip = c("text")) %>%
-  layout(annotations = list(x = 1, y = -0.1, text = "Source: National Agricultural Statistics Service", showarrow = F, xref='paper', yref='paper', 
-      xanchor='right', yanchor='auto'))
-p_21 + theme(axis.text.y = element_text(hjust = 0))
+  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type")
+ggplotly(p_21, tooltip = c("text"))
 
 g_21 <- croplayer1 %>% 
   filter(County == "Goochland", Year==2021) %>%
@@ -32,11 +29,8 @@ ggplot(aes(x = reorder(`Combined`, `Area.Acre`), y = `Area.Acre`, fill = `Area.A
   theme(axis.text.y = element_text(hjust=0)) +
   theme(legend.position = "none") +     
   scale_fill_viridis() + 
-  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type") %>% 
-plot_ly(g_21, tooltip = c("text")) %>%
-  layout(annotations = list(x = 1, y = -0.1, text = "Source: National Agricultural Statistics Service", showarrow = F, xref='paper', yref='paper', 
-      xanchor='right', yanchor='auto'))
-g_21 + theme(axis.text.y = element_text(hjust = 0))
+  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type") 
+ggplotly(g_21, tooltip = c("text")) 
 
 p_12 <- croplayer1 %>% 
   filter(County == "Powhatan", Year== 2012) %>%
@@ -47,11 +41,8 @@ p_12 <- croplayer1 %>%
   theme(axis.text.y = element_text(hjust=0)) +
   theme(legend.position = "none") + 
   scale_fill_viridis() + 
-  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type") %>% 
-plot_ly(p_12, tooltip = c("text")) %>%
-  layout(annotations = list(x = 1, y = -0.1, text = "Source: NASS", showarrow = F, xref='paper', yref='paper', 
-      xanchor='right', yanchor='auto'))
-p_12 + theme(axis.text.y = element_text(hjust = 0))
+  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type")
+ggplotly(p_12, tooltip = c("text")) 
 
 g_12 <- croplayer1 %>% 
   filter(County == "Goochland", Year== 2012) %>%
@@ -62,8 +53,6 @@ g_12 <- croplayer1 %>%
   theme(axis.text.y = element_text(hjust=0)) +
   theme(legend.position = "none") + 
   scale_fill_viridis() + 
-  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type") %>% 
-plot_ly(g_12, tooltip = c("text")) %>%
-  layout(annotations = list(x = 1, y = -0.1, text = "Source: NASS", showarrow = F, xref='paper', yref='paper', 
-      xanchor='right', yanchor='auto'))
-g_12 + theme(axis.text.y = element_text(hjust = 0))
+  labs( title = "Total Acreage by Land type", x = "Acreage", y = "Land type")
+ggplotly(g_12, tooltip = c("text")) 
+
